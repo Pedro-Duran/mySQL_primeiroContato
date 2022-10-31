5746e97d0a600214d3d0a4a854ef67c4cfc98c6b
Flavia-Duran <duranpepeu@gmail.com>
2022-10-31 17:06:46 -0300

    primeira conexao com o mysql

---
+++ LojaVirtaul/src/TestaConexao.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+public static void main(String[] args) throws SQLException {
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+	
+				connection.close();
+}}
+
+
+
