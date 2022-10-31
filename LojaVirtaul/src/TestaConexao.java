6b625e2a9b79f062c08121a8e7fc13628513f6f8
Flavia-Duran <duranpepeu@gmail.com>
2022-10-31 20:21:28 -0300

    interagindo pela primeira vez com a db atraves do java

--- LojaVirtaul/src/TestaConexao.java
+++ LojaVirtaul/src/TestaConexao.java
@@ -1,15 +1,21 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaConexao {
 
 public static void main(String[] args) throws SQLException {
 		
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
-	
-				connection.close();
-}}
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		 Connection connection = connectionFactory.recuperarConexao();
 
+		 System.out.println("fechando conexao!!");
+		 connection.close();
+	}
+
+}
 
 
