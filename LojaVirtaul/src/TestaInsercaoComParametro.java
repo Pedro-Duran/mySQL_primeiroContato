c7c571bd033ef225b34b8519826e061484312122
Flavia-Duran <duranpepeu@gmail.com>
2022-11-03 20:04:47 -0300

    Adquirindo o controle da Transação através .setAutoCommit

--- LojaVirtaul/src/TestaInsercaoComParametro.java
+++ LojaVirtaul/src/TestaInsercaoComParametro.java
@@ -8,29 +8,40 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		 Connection connection = connectionFactory.recuperarConexao();
-		 
-		 String nome = "Mouse'";
-		 String descricao = " Mouse sem fio); delete from Produto;";
-		 
-		 PreparedStatement stm =  connection.prepareStatement("INSERT PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-		 
-		 stm.setString(1, nome);
-		 stm.setString(2, descricao);
-		 stm.execute();
-		 
-		 
-		 ResultSet rst = stm.getGeneratedKeys();
-		 
-		 while(rst.next()) { 
-			 Integer id  = rst.getInt(1);
-			 System.out.println("o id criado foi:" + id);
-		 }
-		 
-		 
-		 
+		Connection connection = connectionFactory.recuperarConexao();
+		connection.setAutoCommit(false);
+
+		try {
+			PreparedStatement stm = connection.prepareStatement("INSERT PRODUTO (nome, descricao) VALUES (?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			adicionarVariavel("SmartTv", "45 polegadas", stm);
+			adicionarVariavel("Radio", "Radio de bateria", stm);
+
+			connection.commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			connection.rollback();
+		}
 	}
 
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+
+		stm.execute();
+		if (nome.equals("Radio"))
+			throw new RuntimeException("Não foi possível adicionar o produto");
+		ResultSet rst = stm.getGeneratedKeys();
+
+		while (rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("o id criado foi:" + id);
+		}
+
+		rst.close();
 	}
 
-
+}
